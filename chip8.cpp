0095fe6d308f416021fb30db3f495604e065dc41
Dalton Kajander <daltonkajander@yahoo.com>
2025-03-08 15:38:39 -0500

    Information about CHIP-8

--- chip8.cpp
+++ chip8.cpp
@@ -1,7 +1,19 @@
+/* 
+Author: Dalton Kajander
+POC: daltonkajander@yahoo.com
+
+Source of information: https://en.wikipedia.org/wiki/CHIP-8#Memory
+*/
 #include <iostream>
 
 int main() {
-	std::cout << "Hello world!" << "\n";
+	// Memory: 4096 memory locations in, all of which are bytes
+	// Registers: 16 8-bit registers, V0-VF
+	// Stack: RCA 1802 version allocates 48 bytes for 12 levels of nesting
+	// Timers: CHIP-8 has two timers, both count down from 60 Hz, the delay timer and sound timer
+	// Input: Hex keybord, 16 keys randing from 0 to F
+	// Graphics: 64x32 pixels, monochrome. 
+	
 	
 	return 0;
 }
